050b47334d02ba0032cf09d252d47a45dbe3fc11
Arun Maiya <arun@maiya.net>
2021-06-10 19:31:57 -0400

    updates

--- causalnlp/autocoder.py
+++ causalnlp/autocoder.py
@@ -130,6 +130,7 @@ class Autocoder:
     def _format_to_df(self, results, df):
         d = {}
         for e in results:
+            if isinstance(e, dict): e = e.items()
             for tup in e:
                 label = tup[0]
                 prob = tup[1]
@@ -148,7 +149,7 @@ class Autocoder:
         return df
 
 
-    def sentiment(self, docs, df, batch_size=8, binarize=False, threshold=0.5):
+    def code_sentiment(self, docs, df, batch_size=8, binarize=False, threshold=0.5):
         """
         Autocodes text for positive or negative sentiment
         """
@@ -160,12 +161,35 @@ class Autocoder:
         if binarize: df = self._binarize_df(df, labels, threshold=threshold)
         return df
 
+    def code_emotion(self, docs, df, batch_size=8, binarize=False, threshold=0.5):
+        """
+        Autocodes text for emotion
+        """
+        labels = ['joy', 'anger', 'fear', 'sadness']
+        results = self.zsl.predict(docs, labels=labels, include_labels=True, multilabel=False,
+                              batch_size=batch_size,
+                              nli_template="The emotion of this text is {}.")
+        df= self._format_to_df(results, df)
+        if binarize: df = self._binarize_df(df, labels, threshold=threshold)
+        return df
 
-
-    def custom_topic(self, docs, df, labels, batch_size=8, binarize=False, threshold=0.5):
+    def code_custom_topics(self, docs, df, labels, batch_size=8, binarize=False, threshold=0.5):
         """
         Autocodes text for user-specified topics.
         The `label` field is the name of the topic as a string (or a list of them.)
+        """
+
+        results = self.zsl.predict(docs, labels=labels, include_labels=True, batch_size=8)
+        df = self._format_to_df(results, df)
+        if binarize: df = self._binarize_df(df, labels, threshold=threshold)
+        return df
+
+    def code_callable(self, docs, df, fn):
+        """
+        Autocodes text for any user-specified function
+        The `fn` parameter must be a Callable and return a dictionary for each
+        text in `docs` where the keys are desired column names and values are scores
+        or probabilities.
         """
 
         results = self.zsl.predict(docs, labels=labels, include_labels=True, batch_size=8)
