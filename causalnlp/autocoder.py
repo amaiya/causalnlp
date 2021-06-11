2baabc86820b780bd7f454ab595be0b2512b5d94
Arun Maiya <arun@maiya.net>
2021-06-11 13:11:00 -0400

    misc updates

--- causalnlp/autocoder.py
+++ causalnlp/autocoder.py
@@ -17,7 +17,7 @@ def list2chunks(a, n):
 
 class ZeroShotClassifier():
     """
-    interface to Zero Shot Topic Classifier
+    Interface to Zero Shot Topic Classifier
     """
 
     def __init__(self, model_name='facebook/bart-large-mnli', device=None):
@@ -45,21 +45,26 @@ class ZeroShotClassifier():
                max_length=512, batch_size=8, nli_template='This text is about {}.',  topic_strings=[]):
         """
         This method performs zero-shot text classification using Natural Language Inference (NLI).
-        Args:
-          docs(list|str): text of document or list of texts
-          labels(list): a list of strings representing topics of your choice
-                        Example:
-                          labels=['political science', 'sports', 'science']
-          include_labels(bool): If True, will return topic labels along with topic probabilities
-          multilabel(bool): If True, labels are considered independent and multiple labels can predicted true for document and be close to 1.
+
+
+        **Parameters**:
+          - docs(list|str): text of document or list of texts
+          - labels(list): a list of strings representing topics of your choice
+                          Example:
+                           labels=['political science', 'sports', 'science']
+          - include_labels(bool): If True, will return topic labels along with topic probabilities
+          - multilabel(bool): If True, labels are considered independent and multiple labels can predicted true for document and be close to 1.
                             If False, scores are normalized such that probabilities sum to 1.
-          max_length(int): truncate long documents to this many tokens
-          batch_size(int): batch_size to use. default:8
+          - max_length(int): truncate long documents to this many tokens
+          - batch_size(int): batch_size to use. default:8
                            Increase this value to speed up predictions - especially
                            if len(topic_strings) is large.
-          nli_template(str): labels are inserted into this template for use as hypotheses in natural language inference
-          topic_strings(list): alias for labels parameter for backwards compatibility
-        Returns:
+          - nli_template(str): labels are inserted into this template for use as hypotheses in natural language inference
+          - topic_strings(list): alias for labels parameter for backwards compatibility
+
+        **Returns:**
+
+
           inferred probabilities or list of inferred probabilities if doc is list
         """
 
@@ -121,7 +126,7 @@ class Autocoder:
     """
     def __init__(self, verbose=1):
         """
-        constructor
+        Instantiates the Autocoder instance.
         """
         self.v = verbose
         self.zsl = ZeroShotClassifier()
